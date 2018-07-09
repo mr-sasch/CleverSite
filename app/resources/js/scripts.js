be073d022f5aa5a4b86a5d2453282ec98ffcee50
Alexander <alex.m.sky@gmail.com>
2018-07-09 12:21:16 +0300

    пагинация отзывы - done

--- app/resources/js/scripts.js
+++ app/resources/js/scripts.js
@@ -4,25 +4,22 @@ galleryBtnRight = document.querySelector('.main-gallery .main-gallery__arrows-ar
 reviewBtnLeft = document.querySelector('.main-reviews .main-gallery__arrows-arrow-left'),
 reviewBtnRight = document.querySelector('.main-reviews .main-gallery__arrows-arrow-right');
 
-reviewBtnLeft.onclick = () => {
-  reviewHiddenLeft = document.querySelector('.main-reviews .slick-prev');
-  reviewHiddenLeft.click();
-};
-reviewBtnRight.onclick = () => {
-  reviewHiddenRight = document.querySelector('.main-reviews .slick-next');
-  reviewHiddenRight.click();
-};
-
-// счетчик пагинации галереи
+// счетчик пагинации
 window.onload = (e) => {
-    // общее количество вкладок
+  // общее количество вкладок Галереи
   var galleryItemCountAllField = document.querySelector('.main-gallery .main-gallery__arrows-center-all'),
   galleryItemLgth = document.querySelectorAll('.main-gallery .slick-slide:not(.slick-cloned)').length;
-
   galleryItemCountAllField.innerHTML = galleryItemLgth;
+
+  // общее количество вкладок Отзывов
+  var reviewsItemCountAllField = document.querySelector('.main-reviews .main-gallery__arrows-center-all'),
+  reviewsItemLgth = document.querySelectorAll('.main-reviews .slick-slide:not(.slick-cloned)').length;
+  reviewsItemCountAllField.innerHTML = reviewsItemLgth;
+
   galleryItem();
+  reviewsItem();
 }
-// текущая вкладка
+// текущая вкладка галереи
 const galleryItem = () => {
   var target = document.querySelector('.main-gallery .slick-current'),
   galleryItems = document.querySelectorAll('.main-gallery .slick-slide:not(.slick-cloned)'),
@@ -36,7 +33,6 @@ const galleryItem = () => {
   }
   galleryItemCountCurrentField.innerHTML = galleryItemCountCurrentValue;
 }
-
 galleryBtnLeft.onclick = () => {
   var galleryHiddenLeft = document.querySelector('.main-gallery .slick-prev');
   galleryHiddenLeft.click();
@@ -46,4 +42,29 @@ galleryBtnRight.onclick = () => {
   var galleryHiddenRight = document.querySelector('.main-gallery .slick-next');
   galleryHiddenRight.click();
   galleryItem();
+};
+
+// текущая вкладка отзывов
+const reviewsItem = () => {
+  var target = document.querySelector('.main-reviews .slick-current'),
+  reviewsItems = document.querySelectorAll('.main-reviews .slick-slide:not(.slick-cloned)'),
+  reviewsItemCountCurrentField = document.querySelector('.main-reviews .main-gallery__arrows-center-current');
+
+  for(var i=0; i<reviewsItems.length; i++) {
+    if (target === reviewsItems[i]) {
+      var reviewsItemCountCurrentValue = i + 1;
+      break;
+    }
+  }
+  reviewsItemCountCurrentField.innerHTML = reviewsItemCountCurrentValue;
+}
+reviewBtnLeft.onclick = () => {
+  reviewHiddenLeft = document.querySelector('.main-reviews .slick-prev');
+  reviewHiddenLeft.click();
+  reviewsItem();
+};
+reviewBtnRight.onclick = () => {
+  reviewHiddenRight = document.querySelector('.main-reviews .slick-next');
+  reviewHiddenRight.click();
+  reviewsItem();
 };
